0ab427c1b8d5842b429339541690b130e3e076fe
HaykTarkhanyan <hayk_tarkhanyan@outlook.com>
2019-08-23 06:46:31 +0000

    making PEP8 valid

--- Digit_classifier/predict.py
+++ Digit_classifier/predict.py
@@ -1,19 +1,20 @@
+import os
 import cv2
 import argparse
 import numpy as np
-import os
 from check_input import check_input
-
-from keras.preprocessing.image import img_to_array
 from keras.models import model_from_json
+from keras.preprocessing.image import img_to_array
+
+LOAD_MODEL_FROM = 'ckpt'
 
 # load json and create model
-json_file = open(os.path.join('ckpt', 'model_cnn.json'), 'r')
+json_file = open(os.path.join(LOAD_MODEL_FROM, 'model_cnn.json'), 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights(os.path.join('ckpt', 'model_cnn.h5'))
+loaded_model.load_weights(os.path.join(LOAD_MODEL_FROM, 'model_cnn.h5'))
 print("Loaded model from ckpt folder")
 
 
@@ -25,6 +26,7 @@ parser.add_argument('-inp_dir', type=str, required=True,
 args = parser.parse_args()
 path = args.inp_dir
 
+# before trying to predict making sure that directory is valid
 if check_input(path):
     im = cv2.imread(path)
     im.resize(28, 28, 1)
