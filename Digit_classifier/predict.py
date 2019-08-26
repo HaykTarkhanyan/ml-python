8b7117de0677c3337c7d9c3066ddb24bf2fceda4
HaykTarkhanyan <hayk_tarkhanyan@outlook.com>
2019-08-26 06:13:23 +0000

    Update Digit_classifier/predict.py

--- Digit_classifier/predict.py
+++ Digit_classifier/predict.py
@@ -1,11 +1,4 @@
-import os
-import cv2
-import argparse
-import numpy as np
 from utils import helpers
-from check_input import check_input
-from keras.models import model_from_json
-from keras.preprocessing.image import img_to_array
 
 # loading model from specified fodler
 LOAD_MODEL_FROM = 'ckpt'
