c07871987458c061b32be9b767080e4100935d56
HaykTarkhanyan <hayk_tarkhanyan@outlook.com>
2019-08-26 12:47:22 +0000

    Update predict.py

--- Icon_classifier/predict.py
+++ Icon_classifier/predict.py
@@ -1,24 +1,40 @@
+import cv2
+import argparse
+from check_input import check_input
 import numpy as np
-from utils import helpers
+from keras.models import model_from_json
+from keras.preprocessing.image import img_to_array
+import os
 
-CLASSES = sorted(['facebook', 'twitter', 'whatsapp', 'linkedin', 'reddit'])
+classes = sorted(['facebook', 'twitter', 'whatsapp', 'linkedin', 'reddit'])
 
-# loading the model
-LOAD_MODEL_FROM = 'ckpt'
-loaded_model = helpers.load_model(LOAD_MODEL_FROM)
+# load json and create model
+json_file = open(os.path.join('ckpt', 'model.json'),  'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights(os.path.join("ckpt", "model.h5"))
+print("Loaded model")
 
 
-# getting input from terminal
-args = helpers.argument_parser()
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-inp_dir', type=str, required=True,
+                    help="specify path to image")
+
+args = parser.parse_args()
 path = args.inp_dir
 
-# checking image is valid or not
-if helpers.check_input(path):
-    im = helpers.convert_image(path)
-    # print the prediction
+if check_input(path):
+    im = cv2.imread(path)
+    im.resize(25, 25, 3)
+    im = img_to_array(im)
+    im = np.array([im])
+
     predic = loaded_model.predict(im)
-    print(CLASSES[np.array(predic).argmax()])
+    print()
+    print(classes[np.array(predic).argmax()])
 
 else:
-    print ("FAILED TO LOAD THE IMAGE")
-
+    check_input(path)
