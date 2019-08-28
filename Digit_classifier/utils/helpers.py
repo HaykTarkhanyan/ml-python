bdb98b0dd513fdab19c4cf8220bd5cc71b272ed2
HaykTarkhanyan <hayk_tarkhanyan@outlook.com>
2019-08-28 11:23:51 +0000

    removing all functions except check_input and convert_image

--- Digit_classifier/utils/helpers.py
+++ Digit_classifier/utils/helpers.py
@@ -1,14 +1,5 @@
-import os
 import cv2
-import argparse
 import numpy as np
-from utils import helpers
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.models import Sequential
-from keras.models import model_from_json
-from keras.preprocessing.image import img_to_array
-from keras.layers import Conv2D, Dropout, MaxPooling2D
 
 
 def __init__():
@@ -24,61 +15,6 @@ def check_input(path):
         print("Please check your input directory")
         return False
     return True
-
-def load_model(folder):
-    # load json and create model
-    json_file = open(os.path.join(folder, 'model_cnn.json'), 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    # load weights into new model
-    loaded_model.load_weights(os.path.join(folder, 'model_cnn.h5'))
-    print("Loaded model from ckpt folder")
-    return loaded_model
-
-def save_model(folder):        
-    # serialize model to JSON
-    model_json = model.to_json()
-    with open(os.path.join(folder, "model.json"), "w") as json_file:
-        json_file.write(model_json)
-    # serialize weights to HDF5
-    model.save_weights(os.path.join(folder, "model.h5"))
-    print("Saved model to ckpt folder")
-
-
-def argument_parser_for_train():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-model_save_dir', type=str, required=False,
-                        help="specify location where model will be saved",
-                        default='pretrained_model')
-    parser.add_argument('-data_dir', type=str, required=False,
-                        help="specify path to data",
-                        default='data')
-    parser.add_argument('-epochs', type=str, required=False,
-                        help="specify number of epochs to train",
-                        default='7')
-    parser.add_argument('-batch_size', type=str, required=False,
-                        help="specify batch_size",
-                        default='1')
-
-    args = parser.parse_args()
-    return args
-
-
-def argument_parser_for_test():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-load_model_from', type=str, required=False,
-                        help="specify location of the model to be loaded",
-                        default='pretrained_model')
-    parser.add_argument('-input_image_dir', type=str, required=True,
-                        help="specify path to data",
-                        )
-
-
-    args = parser.parse_args()
-    return args
 
 
 def convert_image(path):
