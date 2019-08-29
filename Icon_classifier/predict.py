881997d85dd201f0f4f11be548ee1850371b424d
HaykTarkhanyan <hayk_tarkhanyan@outlook.com>
2019-08-29 09:11:57 +0000

    Update Icon_classifier/predict.py

--- Icon_classifier/predict.py
+++ Icon_classifier/predict.py
@@ -9,23 +9,41 @@ from keras.preprocessing.image import img_to_array
 args = helpers.argument_parser_for_test()
 
 CLASSES = sorted(['facebook', 'twitter', 'whatsapp', 'linkedin', 'reddit'])
-LOAD_MODEL_FROM = args.load_model_from
+LOAD_MODEL_FROM = "weights_and_config"
 
+def argument_parser():
+    # get inputs from terminal
+    parser = argparse.ArgumentParser()
 
-# load json and create model
-json_file = open(os.path.join(LOAD_MODEL_FROM),  'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights(os.path.join(LOAD_MODEL_FROM))
-print("Loaded model")
+    parser.add_argument('-load_model_from', type=str, required=False,
+                        help="specify location of the model to be loaded",
+                        default='pretrained_model')
+    parser.add_argument('-input_image_dir', type=str, required=True,
+                        help="specify path to data",
+                        )
+    
+    args = parser.parse_args()
+    return args
 
+def load_model():
+    # load json and create model
+    json_file = open(os.path.join(LOAD_MODEL_FROM),  'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(os.path.join(LOAD_MODEL_FROM))
+    print("Loaded model")
 
-
-path = args.input_image_dir
+    return loaded_model
 
 if check_input(path):
+    # parse arguments 
+    args = argument_parser()
+    path = args.input_image_dir
+    # load model
+    loaded_model = load_model()
+    # convert image
     im = helpers.convert_image(path)
     # print prediction with higheset probability
     predic = loaded_model.predict(im)
